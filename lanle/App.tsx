f5309fa0e0e3a041ff1a557776406fee34f37297
Cactus322 <momchyan1997@mail.ru>
2023-07-25 23:18:48 +0300

    create connection from openAI

--- lanle/App.tsx
+++ lanle/App.tsx
@@ -1,20 +1,43 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from 'expo-status-bar'
+import { Configuration, OpenAIApi } from 'openai'
+import { StyleSheet, Text, View, useWindowDimensions } from 'react-native'
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+	const apiKey = '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'
+
+	const hoba = async () => {
+		const configuration = new Configuration({
+			apiKey: apiKey,
+		})
+
+		const openai = new OpenAIApi(configuration)
+
+		const chatCompletion = await openai.createChatCompletion({
+			model: 'gpt-3.5-turbo',
+			messages: [{ role: 'user', content: 'Ты можешь перевести текст?' }],
+		})
+		console.log(chatCompletion.data.choices[0].message)
+	}
+
+	hoba()
+
+	const { width, height } = useWindowDimensions();
+
+	return (
+		<>
+			<View style={styles.container}>
+				<Text>Open up App.tsx to start working on your app!</Text>
+				<StatusBar style="auto" />
+			</View>
+		</>
+	)
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+	container: {
+		flex: 1,
+		backgroundColor: '#fff',
+		alignItems: 'center',
+		justifyContent: 'center',
+	},
+})
